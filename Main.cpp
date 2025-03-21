50312ddb90c1304e87fa003c6072860abf818013
mmd <m.allavvi712@gmail.com>
2025-03-21 14:20:53 +0330

    fixed printing & navigating

--- Main.cpp
+++ Main.cpp
@@ -1,114 +1,203 @@
 #include "clui.h"
 #include <ctime>
 
-// Define Sudoku table size
 #define SIZE 9
 
-// Predefined Sudoku tables
+// 10 prebuilt Sudoku puzzles.
+// A 0 represents an empty cell.
 int sudoku[10][SIZE][SIZE] = {
-    {{5, 3, 0, 0, 7, 0, 0, 0, 0},
-     {6, 0, 0, 1, 9, 5, 0, 0, 0},
-     {0, 9, 8, 0, 0, 0, 0, 6, 0},
-     {8, 0, 0, 0, 6, 0, 0, 0, 3},
-     {4, 0, 0, 8, 0, 3, 0, 0, 1},
-     {7, 0, 0, 0, 2, 0, 0, 0, 6},
-     {0, 6, 0, 0, 0, 0, 2, 8, 0},
-     {0, 0, 0, 4, 1, 9, 0, 0, 5},
-     {0, 0, 0, 0, 8, 0, 0, 7, 9}},
-    
+    // Puzzle 1
+    {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    },
+    // Puzzle 2
+    {
+        {0, 0, 0, 2, 6, 0, 7, 0, 1},
+        {6, 8, 0, 0, 7, 0, 0, 9, 0},
+        {1, 9, 0, 0, 0, 4, 5, 0, 0},
+        {8, 2, 0, 1, 0, 0, 0, 4, 0},
+        {0, 0, 4, 6, 0, 2, 9, 0, 0},
+        {0, 5, 0, 0, 0, 3, 0, 2, 8},
+        {0, 0, 9, 3, 0, 0, 0, 7, 4},
+        {0, 4, 0, 0, 5, 0, 0, 3, 6},
+        {7, 0, 3, 0, 1, 8, 0, 0, 0}
+    },
+    // Puzzle 3
+    {
+        {0, 2, 0, 6, 0, 8, 0, 0, 0},
+        {5, 8, 0, 0, 0, 9, 7, 0, 0},
+        {0, 0, 0, 0, 4, 0, 0, 0, 0},
+        {3, 7, 0, 0, 0, 0, 5, 0, 0},
+        {6, 0, 0, 0, 0, 0, 0, 0, 4},
+        {0, 0, 8, 0, 0, 0, 0, 1, 3},
+        {0, 0, 0, 0, 2, 0, 0, 0, 0},
+        {0, 0, 9, 8, 0, 0, 0, 3, 6},
+        {0, 0, 0, 3, 0, 6, 0, 9, 0}
+    },
+    // Puzzle 4
+    {
+        {0, 0, 0, 0, 0, 0, 2, 0, 0},
+        {0, 8, 0, 0, 0, 7, 0, 9, 0},
+        {6, 0, 2, 0, 0, 0, 5, 0, 0},
+        {0, 7, 0, 0, 6, 0, 0, 0, 0},
+        {0, 0, 0, 9, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 2, 0, 0, 4, 0},
+        {0, 0, 5, 0, 0, 0, 6, 0, 3},
+        {0, 9, 0, 4, 0, 0, 0, 7, 0},
+        {0, 0, 6, 0, 0, 0, 0, 0, 0}
+    },
+    // Puzzle 5
+    {
+        {0, 0, 0, 0, 0, 0, 0, 1, 2},
+        {0, 0, 0, 0, 3, 5, 0, 0, 0},
+        {0, 0, 8, 0, 0, 0, 4, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0},
+        {3, 0, 0, 0, 0, 0, 0, 0, 8},
+        {0, 0, 0, 0, 0, 0, 0, 2, 0},
+        {0, 0, 1, 0, 0, 0, 7, 0, 0},
+        {0, 0, 0, 3, 8, 0, 0, 0, 0},
+        {2, 4, 0, 0, 0, 0, 0, 0, 0}
+    },
+    // Puzzle 6
+    {
+        {1, 0, 0, 0, 0, 7, 0, 9, 0},
+        {0, 3, 0, 0, 2, 0, 0, 0, 8},
+        {0, 0, 9, 6, 0, 0, 5, 0, 0},
+        {0, 0, 5, 3, 0, 0, 9, 0, 0},
+        {0, 1, 0, 0, 8, 0, 0, 0, 2},
+        {6, 0, 0, 0, 0, 4, 0, 0, 0},
+        {3, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 4, 0, 0, 0, 0, 0, 0, 7},
+        {0, 0, 7, 0, 0, 0, 3, 0, 0}
+    },
+    // Puzzle 7
+    {
+        {0, 2, 0, 0, 0, 0, 0, 6, 0},
+        {0, 0, 0, 0, 0, 3, 0, 0, 5},
+        {0, 1, 0, 0, 8, 0, 9, 0, 0},
+        {8, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 7, 0, 0, 0, 0, 0},
+        {0, 0, 6, 0, 0, 0, 0, 0, 1},
+        {0, 0, 2, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 6, 0, 0, 0},
+        {0, 5, 0, 0, 0, 0, 0, 8, 0}
+    },
+    // Puzzle 8
+    {
+        {9, 0, 0, 0, 0, 0, 0, 7, 0},
+        {0, 0, 0, 4, 2, 0, 1, 0, 0},
+        {0, 8, 0, 0, 0, 0, 0, 3, 0},
+        {0, 0, 7, 0, 0, 1, 0, 0, 5},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {6, 0, 0, 7, 0, 0, 8, 0, 0},
+        {0, 7, 0, 0, 0, 0, 0, 5, 0},
+        {0, 0, 9, 0, 3, 6, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 2}
+    },
+    // Puzzle 9
+    {
+        {2, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 7, 0, 0, 0, 9, 0, 0},
+        {0, 0, 0, 4, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 6, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 4, 0, 0, 0},
+        {0, 0, 5, 0, 0, 0, 8, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 3}
+    },
+    // Puzzle 10
+    {
+        {0, 0, 0, 2, 0, 0, 0, 6, 0},
+        {0, 0, 1, 0, 0, 0, 7, 0, 0},
+        {0, 3, 0, 0, 9, 0, 0, 0, 8},
+        {0, 0, 0, 7, 0, 0, 0, 0, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {0, 0, 0, 0, 0, 4, 0, 0, 0},
+        {7, 0, 0, 0, 2, 0, 0, 5, 0},
+        {0, 0, 6, 0, 0, 0, 1, 0, 0},
+        {0, 8, 0, 0, 0, 9, 0, 0, 0}
+    }
 };
 
-// Function to select a random Sudoku table
-void get_random_sudoku(int table[SIZE][SIZE])
-{
+// Selects a random Sudoku puzzle from the array.
+void get_random_sudoku(int table[SIZE][SIZE]) {
     srand((unsigned int)time(nullptr));
     int randomIndex = rand() % 10;
     for (int i = 0; i < SIZE; i++)
-    {
         for (int j = 0; j < SIZE; j++)
-        {
             table[i][j] = sudoku[randomIndex][i][j];
-        }
-    }
 }
 
-// Function to print the Sudoku grid
-void print_sudoku(int table[SIZE][SIZE])
-{
+// Prints (and refreshes) the Sudoku grid using clui functions.
+void print_sudoku(int table[SIZE][SIZE]) {
     clear_screen();
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
-            cursor_to_pos(i + 1, j * 4 + 1); // Positioning each cell
-            if (table[i][j] == 0)
-            {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cursor_to_pos(i + 1, j * 4 + 1);
+            if (table[i][j] == 0) {
                 change_color(COLOR_CYAN);
                 printf(" . ");
-            }
-            else
-            {
+            } else {
                 change_color(COLOR_GREEN);
                 printf("%3d", table[i][j]);
             }
         }
-        reset_color();
-        printf("\n");
     }
+    reset_color();
 }
 
-// Function to navigate and edit Sudoku table
-void play_sudoku(int table[SIZE][SIZE])
-{
+// Allows navigation with WASD and lets the user change cell values.
+// Press 'q' to quit.
+void play_sudoku(int table[SIZE][SIZE]) {
     int x = 0, y = 0;
     char ch;
-    while (true)
-    {
+    while (true) {
+        print_sudoku(table);
+        // Highlight the current cell
         cursor_to_pos(y + 1, x * 4 + 1);
         change_color(COLOR_RED);
-        printf("%3d", table[y][x]);
+        if (table[y][x] == 0)
+            printf(" . ");
+        else
+            printf("%3d", table[y][x]);
         reset_color();
 
         ch = getch();
-        switch (ch)
-        {
-        case 'w': // Move up
-            if (y > 0)
-                y--;
-            break;
-        case 's': // Move down
-            if (y < SIZE - 1)
-                y++;
-            break;
-        case 'a': // Move left
-            if (x > 0)
-                x--;
-            break;
-        case 'd': // Move right
-            if (x < SIZE - 1)
-                x++;
-            break;
-        case 'q': // Quit
-            return;
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            table[y][x] = ch - '0';
-            break;
+        switch (ch) {
+            case 'w': // move up
+                if (y > 0) y--;
+                break;
+            case 's': // move down
+                if (y < SIZE - 1) y++;
+                break;
+            case 'a': // move left
+                if (x > 0) x--;
+                break;
+            case 'd': // move right
+                if (x < SIZE - 1) x++;
+                break;
+            case 'q': // quit
+                return;
+            case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+                table[y][x] = ch - '0';
+                break;
+            default:
+                break;
         }
-        print_sudoku(table);
     }
 }
 
-// Main function
-int main()
-{
+int main() {
     init_clui();
     int table[SIZE][SIZE];
     get_random_sudoku(table);
